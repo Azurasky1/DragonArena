2f2599c63961687cf943cb32eafbc813c838f989
Andrea Sonny <andreasonny83@gmail.com>
2016-04-15 22:38:48 +0100

    introducing a user interaction page

---
+++ app/scripts/player.js
@@ -0,0 +1,14 @@
+(function(global, doc, undefined) {
+  'use strict';
+
+  var _player = {
+    pos: {
+      x: 0,
+      y: 0
+    },
+
+    avatar: ''
+  };
+
+  window.Player = _player;
+})(window | self, document);
