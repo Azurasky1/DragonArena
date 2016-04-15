8c4dbb86d30f19e1db6fbe3f11ac0673d16808a6
Azurasky1 <tarutaruwhm@live.com>
2016-04-15 15:55:54 -0400

    Added files via upload
    here is the animation that i wrote, like i mentioned in my email this is very inefficient because it consumes too many resources. requestAnimationFrame() will generally be much better because if a user tabs out it the entire loop will slow, among many other reasons

---
+++ alertuser.js
@@ -0,0 +1,10 @@
+function alertUser () {
+alert("The code works");
+}
+function writeTable () {
+var para = document.createElement("p");
+//inserts variable dayweek into paragraph element
+var node = document.createTextNode("it's working");
+para.appendChild(node);
+//changes the day of the week as we create more table headers	
+	}
