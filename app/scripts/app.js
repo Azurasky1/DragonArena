f21d10bd2813e02c203695690aec97df105009ce
Andrea Sonny <andreasonny83@gmail.com>
2016-04-16 14:45:33 +0100

    first animations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,9 @@
   var c = document.getElementById('canvas');
   var csv = c.getContext('2d');
 
-  var frameRate = 10;
+  var frameRate = 5;
+  var frameCounter = 0;
+
   var screenHeight = document.body.offsetHeight;
   var screenWidth = document.body.offsetWidth;
 
@@ -14,11 +16,21 @@
       y: 0
     },
 
-    avatar: 'images/players/001.png'
-  };
+    animation: {
+      x: 0,
+      y: 0
+    },
 
-  var avatar = new Image();
-  avatar.src = player.avatar;
+    frame: {
+      width: 56,
+      height: 64,
+      frames: 9,
+      current_frame: 0
+    },
+
+    avatar: new Image(),
+    avatar_src: 'images/players/001.png'
+  };
 
   // shim layer with setTimeout fallback
   global.requestAnimFrame = (function() {
@@ -30,33 +42,47 @@
       };
   })();
 
+  // keyboard listener
   global.onkeydown = function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
 
     if (key == 38) {
       // key up
-
+      player.animation.x = 0;
+      player.animation.y = 65;
     } else if (key == 40) {
       // key down
-
+      player.animation.x = 0;
+      player.animation.y = 391;
     } else if (key == 37) {
       // key left
-
+      player.animation.x = 0;
+      player.animation.y = 260;
     } else if (key == 39) {
       // key right
-
+      player.animation.x = 0;
+      player.animation.y = 194;
     }
   }
 
   function gameLoop() {
     // Borad
-    $BOARD.drawBoard(csv, screenWidth, screenHeight);
+    $GAME.drawBoard(csv);
 
     // Player
-    $PLAYER.drawPlayer(csv, avatar);
+    $GAME.drawPlayer(csv, player);
+    player = $GAME.nextFrame(player);
   }
 
   function animationLoop() {
+    if ( frameCounter < frameRate ) {
+      frameCounter++;
+      requestAnimFrame(animationLoop);
+      return;
+    }
+
+    frameCounter = 0;
+
     gameLoop();
     requestAnimFrame(animationLoop);
   }
