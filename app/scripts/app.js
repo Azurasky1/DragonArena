8993c0ffaf8df36684ecc8e5aab7435184e3d755
Azurasky <tarutaruwhm@live.com>
2016-06-16 08:32:29 -0400

    I cleaned the code by following the code style guide. Added health bars, removed keyboard delay on player movement inputs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,259 +1,182 @@
-(function(global) {
+(function (global) {
   'use strict';
 
+  const KEY_UP = 38;
+  const KEY_DOWN = 40;
+  const KEY_LEFT = 37;
+  const KEY_RIGHT = 39;
   var c = document.getElementById('canvas');
   var csv = c.getContext('2d');
   var keyState = {};
   var keysdown = {};
-  var fps = 15;
   var lastCall = Date.now();
   var screenHeight = document.body.offsetHeight;
   var screenWidth = document.body.offsetWidth;
   var timeBetweenFrames;
+  var frameCount = 0;
+  var fps;
+  var fpsInterval;
+  var startTime;
+  var now;
+  var then;
+  var elapsed;
 
   // set the canvas to the full page size
   c.width = screenWidth;
   c.height = screenHeight;
 
+  // keeps track of which keys are pressed
   var keyState = [];
-  const KEY_UP = 38;
-  const KEY_DOWN = 40;
-  const KEY_LEFT = 37;
-  const KEY_RIGHT = 39;
 
   window.addEventListener('keydown', function (e) {
     keyState[e.keyCode || e.which] = true;
   }, true);
 
-  window.addEventListener('keyup',function(e){
+  window.addEventListener('keyup', function (e) {
     keyState[e.keyCode || e.which] = false;
-},true);
-
-/*
-  onkeydown = window.onkeyup = function (e) {
-      if (e.type === 'keydown') {
-          keyState[e.keyCode] = e.type == 'keydown';
-          console.log("working");
-      }
-  }
-*/
+  }, true);
 
   function Player(totalHealth, currentHealth) {
     this.health = {
       totalHealth: totalHealth,
       currentHealth: currentHealth,
-      percent: (currentHealth / totalHealth) * 100
+      percent: (currentHealth / totalHealth) * 100,
     };
     this.animation = {
       x: 0,
-      y: 0
+      y: 0,
     };
     this.frame = {
       current_frame: 0,
       frames: 9,
-      width: (500/9), // 55.555555555...6
-      height: (519/9) // 57.6666666...7
+      width: (500 / 9), // 55.555555555...6
+      height: (519 / 9), // 57.6666666...7
     };
     this.pos = {
-      x: screenWidth/2 - ((500/9)/2),
-      y: screenHeight/2 - ((519/9)/2)
+      x: screenWidth / 2 - ((500 / 9) / 2),
+      y: screenHeight / 2  - ((519 / 9) / 2),
     };
     this.speed = 1;
     this.avatar;
   };
 
-  Player.prototype.update = function(timeBetweenFrames) {
-    if (keyState[KEY_UP]) {
-      this.animation.y = 0;
-      this.pos.y -= this.speed * timeBetweenFrames;
-    }
-
-    if (keyState[KEY_DOWN]) {
-      this.animation.y = 60;
-      this.pos.y += this.speed * timeBetweenFrames;
-    }
-
-    if (keyState[KEY_LEFT]) {
-      this.animation.y = 120;
+  Player.prototype.update = function (timeBetweenFrames) {
+    if (keyState[38] && keyState[37]) {
       this.pos.x -= this.speed * timeBetweenFrames;
-    }
-
-    if (keyState[KEY_RIGHT]) {
-      this.animation.y = 180;
+      this.pos.y -= this.speed * timeBetweenFrames;
+      this.animation.y = 0;
+    } else if (keyState[38] && keyState[39]) {
+      this.pos.x += this.speed * timeBetweenFrames;
+      this.pos.y -= this.speed * timeBetweenFrames;
+      this.animation.y = 130;
+    }  else if (keyState[40] && keyState[37]) {
+      this.pos.x -= this.speed * timeBetweenFrames;
+      this.pos.y += this.speed * timeBetweenFrames;
+      this.animation.y = 327;
+    }  else if (keyState[40] && keyState[39]) {
+      this.pos.x += this.speed * timeBetweenFrames;
+      this.pos.y += this.speed * timeBetweenFrames;
+      this.animation.y = 454;
+    }  else if (keyState[KEY_UP]) {
+      this.animation.y = 65;
+      this.pos.y -= this.speed * timeBetweenFrames;
+    }  else if (keyState[KEY_DOWN]) {
+      this.animation.y = 391;
+      this.pos.y += this.speed * timeBetweenFrames;
+    }  else if (keyState[KEY_LEFT]) {
+      this.animation.y = 260;
+      this.pos.x -= this.speed * timeBetweenFrames;
+    }  else if (keyState[KEY_RIGHT]) {
+      this.animation.y = 194;
       this.pos.x += this.speed * timeBetweenFrames;
     }
   };
 
   var player = new Player(100, 100);
 
-  function Healthbar (width, height, hex, anyPlayerOrEnemy) {
+  // healthbar constructor
+  // creates healthbar and sets position 20px above the passed in object
+  // for now, all healthbars will be 200px in width, or...
+  // ... twice the health percentage of any given object
+  // hex is just a color, can be hexadecimal or a string
+  // anyPlayerOrEnemy is an object of the Player() class, will add more soon
+  function Healthbar(width, height, hex, anyPlayerOrEnemy) {
     this.width = width;
     this.height = height;
     this.color = hex;
     this.canvasPosition = {
       x: anyPlayerOrEnemy.pos.x,
-      y: anyPlayerOrEnemy.pos.y - height
-    }
+      y: anyPlayerOrEnemy.pos.y - 20,
+    };
   }
 
-  Healthbar.prototype.drawBar = function (anyPlayerOrEnemy) {
+  // all health bars will need their own draw and update functions
+  // here they are in the prototype
+  Healthbar.prototype = {
+    drawBar: function (anyPlayerOrEnemy) {
         if (anyPlayerOrEnemy.health.percent) {
           var num = anyPlayerOrEnemy.health.percent;
           csv.fillStyle = this.color;
-          csv.fillRect = (0, 0, (num / this.width), this.height);
-        }
-        else {
-          console.log("In order to determine the length of this health bar, please input a player" +
-          " or enemy object with a valid .health.percent property.");
+          csv.fillRect(this.canvasPosition.x, this.canvasPosition.y, this.width, this.height);
+        } else {
+          console.log('In order to determine the length of this health bar, please input a player' +
+            ' or enemy object with a valid .health.percent property.');
           return;
         }
-  }
+      },
 
-  Healthbar.prototype.update = function (anyPlayerOrEnemy) {
-    this.canvasPosition.x = anyPlayerOrEnemy.pos.x;
-    this.canvasPosition.y = anyPlayerOrEnemy.pos.y - 10;
-  }
+    update: function (anyPlayerOrEnemy) {
+      this.canvasPosition.x = anyPlayerOrEnemy.pos.x;
+      this.canvasPosition.y = anyPlayerOrEnemy.pos.y - 20;
+      this.width = anyPlayerOrEnemy.health.percent * 2;
+    },
+  };
 
   var playerHealth = new Healthbar(200, 10, 'green', player);
 
-    /**
-     * the avatarSrc property is later assigned to player.avatar.src
-     * (src is a property of an HTMLImageElement)
-     * dimensions: 500x519
-     *
-     * @type {String}
-     */
-
-  // so that the movement animation resets only if the player changes direction
-  /*
-  function checkSpritesheet(frameNow, y) {
-    if ((frameNow > 0) && player.animation.y !== y) {
-      var new_frame = 0;
-      player.frame.currentFrame = new_frame;
-      player.animation.y = y;
-    } else if (frameNow === 0 && player.animation.y !== y) {
-      player.animation.y = y;
-    }
-  }
-*/
-
-/*
-  if (keysdown[38] && keysdown["37"]) {
-    console.log('up & left');
-    player.pos.x -= 4;
-    player.pos.y -= 4;
-    player.animation.y = 0;
-  } else if (keysdown[38] && keysdown[39]) {
-    console.log('up & right');
-    player.pos.x += 4;
-    player.pos.y -= 4;
-    player.animation.y = 130;
-  } else if (keysdown[40] && keysdown[37]) {
-    console.log('down and left');
-    player.pos.x -= 4;
-    player.pos.y += 4;
-    player.animation.y = 327;
-  } else if (keysdown[40] && keysdown[39]) {
-    console.log('down and right');
-    player.pos.x += 4;
-    player.pos.y += 4;
-    player.animation.y = 454;
-  } else if (keysdown[38] === true) {
-    console.log('up');
-    player.animation.y = 64;
-    player.pos.y -= 4;
-    player.animation.y = 65;
-  }
-  else if (keysdown[40] === true) {
-    console.log('down');
-    player.pos.y += 4;
-    player.animation.y = 391;
-  }
-  else if (keysdown[37] === true) {
-    console.log('left');
-    player.pos.x -= 4;
-    player.animation.y = 260;
-  }
-  else if (keysdown[39] === true) {
-    console.log('right');
-    player.pos.x += 4;
-    player.animation.y = 194;
-  }
-*/
-
-/*
-
-  function keyDown(evt) {
-     keysdown[evt.which] = true;
+  function updateEverythingThenDraw(timeBetweenFrames) {
+    player.update(timeBetweenFrames);
+    player = $GAME.nextFrame(player);
+    $GAME.drawBoard(csv);
+    $GAME.drawPlayer(csv, player);
+    player.health.percent -= .1;
+    playerHealth.update(player);
+    playerHealth.drawBar(player);
   }
 
-  function keyUp(evt) {
-    keysdown[evt.which] = false;
+  function startGame(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    tick();
   }
-
-*/
-
-/*
-
-  function Keyboarder() {
-    this.isDown = function(keyCode) {
-      return keyState[keyCode] === true;
-    };
-
-    this.KEYS = {
-      UP: 38,
-      DOWN: 40,
-      LEFT: 37,
-      RIGHT: 39,
-      SPACE: 32
-    };
-  }
-
-*/
 
   function tick() {
 
-	var now = Date.now();
-	timeBetweenFrames = (now - lastCall) / 1000; //time in seconds
-  lastCall = now;
-
-      player.update(timeBetweenFrames);
-      player = $GAME.nextFrame(player);
-      $GAME.drawBoard(csv);
-      $GAME.drawPlayer(csv, player);
-      playerHealth.update(player);
-      playerHealth.drawBar(player);
+    if (player.health.percent < 0) {
+      console.log('You ran out of health! You lose!');
+      return;
+    }  else {
       requestAnimationFrame(tick);
+      now = Date.now();
+      elapsed = now - then;
+
+      if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+        updateEverythingThenDraw(elapsed);
+      }
+    }
   }
-
-/*
-  function startGame() {
-
-    // start the animation loop
-    player.update();
-    $GAME.drawBoard(csv);
-    $GAME.drawPlayer(csv, player);
-
-    window.addEventListener('keydown', keyDown, false);
-
-    window.addEventListener('keyup', keyUp, false);
-  }
-*/
 
   player.avatar = new Image();
   player.avatar.onload = function () {
-    $GAME.drawBoard(csv);
-    $GAME.drawPlayer(csv, player);
-    playerHealth.update(player);
-    playerHealth.drawBar(player);
-    tick();
-  }
+    startGame(30);
+  };
+
   player.avatar.onerror = function () {
-  console.error("Cannot load spritesheet");
-  }
-  player.avatar.src = "/images/players/001.png";
+    console.error('Cannot load spritesheet');
+  };
 
-
-
+  player.avatar.src = '/images/players/001.png';
 
 })(window);
--- app/scripts/modules/game.js
+++ app/scripts/modules/game.js
@@ -1,61 +1,39 @@
-/*
-Comments marked with -B are written by Bill to help better understand the code or ask questions for later
-
-// Notable concepts in this code: a "shim" addressed towards the bottom, if/else shorthand, checking for keyboard presses in user input logic
-// document.body.offsetWidth and document.body.offsetHeight
-// Note: Unlike a typical (x, y) axis, when using a method attached to the 2d context object, the y value of the top of the canvas will start at zero.
-*/
-(function(global, undefined) {
+(function (global, undefined) {
   var screenWidth = document.body.offsetWidth;
   var screenHeight = document.body.offsetHeight;
-
   function game() {
     // private scope
     var _board = {
-      background: '#FAFAFA'
+      background: '#FAFAFA',
     };
 
-    // gameModule is an object, the functions in this file are assigned to this object as methods to be used in the rest of the program
     // public scope
     var gameModule = {};
 
     function drawPlayer(graph, player) {
       // taken from (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage)
-      // this function takes eight parameters. (image, sx, sy, swidth, sHeight, dx, dy, dWidth, dHeight)
       graph.drawImage(
-        // first parameter is equal to the avatar property of the player object found in app.js, with the source property set above. -B
         player.avatar,
-        // first frame taken from (0,0 on spritesheet)
-        // next frame taken starts at (frame.width) because current_frame increments by 1 because of global.onkeydown()
         player.frame.width * player.frame.current_frame,
-        // y will change only if the user changes direction, x will change as a result of the animation -B
         player.animation.y,
-        // size of the image -B
         player.frame.width, player.frame.height, // clip size -S
-        // dx & dy, d meaning destination canvas -B
-        // start position on destination canvas -B
-        player.pos.x, player.pos.y,
-        // centered on canvas
+        player.pos.x, player.pos.y, // centered on canvas
         player.frame.width, player.frame.height // image size to draw
       );
     }
 
     function nextFrame(el) {
-      // very good practice starting at frame 0
-      // once current_frame reaches 8 (totaling 9 frames) it resets to 0. -B
       if (el.frame.current_frame < el.frame.frames - 1) {
         el.frame.current_frame += 1;
-      } else {
+      } else
         el.frame.current_frame = 0;
-      }
-      // returns the same object (player, or whatever other object we use) with an updated current_frame property -B
       return el;
     }
 
     function drawBoard(canvas) {
-      // just fills the canvas with the background color of _board.background -B
+      // added clearRect so that we don't keep drawing over the canvas
+      canvas.clearRect(0, 0, canvas.width, canvas.height);
       canvas.fillStyle = _board.background;
-      // the canvas background starts at the top-left of the canvas, and spans the entire screen width and height, set at the beginning of the IIFE -B
       canvas.fillRect(0, 0, screenWidth, screenHeight);
     }
 
@@ -63,17 +41,10 @@ Comments marked with -B are written by Bill to help better understand the code o
     gameModule.drawBoard = drawBoard;
     gameModule.nextFrame = nextFrame;
 
-    // gameModule object is returned as a result of the game function -B
     return gameModule;
   };
 
-  // this is the only line of code that is run in the IIFE execution context -B
-  // all of the functions assigned to the gameModule object are now made available to the rest of the program using the $GAME object -B
   // expose $PLAYER on the global scope
   global.$GAME = game();
 
-// checks to see if the window object is undefined, if not uses the window (global) object as the first parameter -B
-// if it is undefined then it uses its own "global" object, with all of the methods attached to it -B
-// since all major browsers support the window object, could this be for implementing support on mobile? -B
-// also, what is the undefined parameter for in the IIFE? -B
 })(typeof window !== 'undefined' ? window : global);
